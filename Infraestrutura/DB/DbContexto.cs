6ad3bb847fcd079b37a6b8b0c2f0eb205bc9fc51
Hideviskis <hideki.restani@outlook.com>
2025-10-03 16:44:39 -0300

    adicionando banco de dados ao projeto

---
+++ Infraestrutura/DB/DbContexto.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using minimal_api.Dominio.Entidades;
+
+namespace minimal_api.Infraestrutura.Db;
+
+public class DbContexto : DbContext
+{
+    private readonly IConfiguration _configuracaoAppSettings;
+    public DbContexto(IConfiguration configuracaoAppSettings)
+    {
+        _configuracaoAppSettings = configuracaoAppSettings;
+    }
+    public DbSet<Administrador> Administradores { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var stringConexao = _configuracaoAppSettings.GetConnectionString("mysql")?.ToString();
+        if (!optionsBuilder.IsConfigured)
+        {
+            if (!string.IsNullOrEmpty(stringConexao))
+            {
+                optionsBuilder.UseMySql(
+                    stringConexao,
+                    ServerVersion.AutoDetect(stringConexao));
+            }
+        }
+    }
+} 
