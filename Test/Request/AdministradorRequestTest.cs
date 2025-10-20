7067ef7fb273bb4057af722dc694df4606748bfc
Hideviskis <hideki.restani@outlook.com>
2025-10-20 10:51:39 -0300

    Fix arquivo sln duplicados

--- Test/Request/AdministradorRequestTest.cs
+++ Test/Request/AdministradorRequestTest.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Test.Requests
+namespace Test.Request
 {
     public class AdministradorRequestTest
     {
